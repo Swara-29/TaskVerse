c8fa975de243ae27d49f0bbdcb344e00b7bdd752
pudkeswara29@gmail.com <pudkeswara29@gmail.com>
2026-04-01 20:35:42 +0530

    fix: add export default to db.js

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import taskRoutes from "./routes/taskRoutes.js";
+import https from "https";
+
 
 dotenv.config();
 const app = express();
 
 connectDB();
+
+setInterval(() => {
+  https.get("https://taskverse-lggn.onrender.com/", (res) => {
+    console.log(`Keep alive ping: ${res.statusCode}`);
+  }).on("error", (err) => {
+    console.error("Keep alive error:", err.message);
+  });
+}, 14 * 60 * 1000); // every 14 minutes
 
 // ✅ CORS must be before everything else
 app.use(cors());
